0a77482103f381a17268f5d92ac46e6f549992ee
naglenda31 <naglenda31@gmail.com>
2021-03-19 20:36:30 +0000

    updated exercise 14 code to correct delete task action

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -15,11 +15,15 @@ for (let i = 0; i < deleteIcons.length; i++) {
 function addToDo(item) {
 	let li = document.createElement("li");
 	li.innerHTML = '<span><i class="fa fa-trash"></i></span> ' + item;
-	// deleteIcons.addEventListener("click", e => deleteToDo(e, i));
+
+	let i = list.childNodes.length - 1;
+	console.log(i, list);
+	li.addEventListener("click", e => deleteToDo(e, i));
 	list.appendChild(li);
 	newToDo.value = "";
 }
 
 function deleteToDo(e, i) {
+	console.log(list.childNodes[i]);
 	list.removeChild(list.childNodes[i]);
 }
